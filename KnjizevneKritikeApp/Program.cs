89a77b5b0cbf9593238fb121b445cbaeef8b41af
Luka Stankoivc <lstankovic4122it@raf.rs>
2025-06-18 15:34:01 +0200

    Promena kod Privacy i redirect main straince

--- KnjizevneKritikeApp/Program.cs
+++ KnjizevneKritikeApp/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Recenzije}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
